bfa3b73b09186a04b8e7c5b964c187697620d72d
davidcalles1 <cc0324032019@unab.edu.sv>
2022-03-06 14:32:46 -0600

    Entidades

---
+++ Laboratorio1/src/main/java/Entidades/Contactos.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author nelso
+ */
+public class Contactos {
+    int ID;
+    String Nombre;
+    int Edad;
+    String Email;
+    int NumeroDeTelefono;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public int getNumeroDeTelefono() {
+        return NumeroDeTelefono;
+    }
+
+    public void setNumeroDeTelefono(int NumeroDeTelefono) {
+        this.NumeroDeTelefono = NumeroDeTelefono;
+    }
+    
+    
+    
+}
