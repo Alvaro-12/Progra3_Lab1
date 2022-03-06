d5445ffdc1ba68ec62545ec13662c87f90113a1a
davidcalles1 <cc0324032019@unab.edu.sv>
2022-03-06 15:21:25 -0600

    Dao

---
+++ Laboratorio1/src/main/java/DAO/DContacto.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import ConexionBD.conexionDB;
+import Entidades.Contactos;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author nelso
+ */
+public class DContacto {
+    conexionDB con = new conexionDB ();
+        Connection conexion = con.getConecction();
+    public ArrayList<Contactos>ListadoEstudiantes()       
+    { ArrayList<Contactos> listado = null;
+      
+        try {
+             listado = new ArrayList<Contactos>();
+            
+             
+             CallableStatement cb = conexion.prepareCall("select * from contactos");
+                ResultSet resultado = cb.executeQuery();
+                
+             while (resultado.next())
+             {
+             
+             Contactos ct = new Contactos();
+             ct.setID(resultado.getInt("ID"));
+             ct.setNombre(resultado.getString("Nombre"));
+             ct.setEdad(resultado.getInt("Edad"));
+             ct.setEmail(resultado.getString("Email"));
+             ct.setNumeroDeTelefono(resultado.getInt("NumeroDeTelefono"));
+             listado.add(ct);
+             }   
+                
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+     return listado;
+     
+    }
+    public void AddContacto(Contactos ct)
+    {
+            try {
+                CallableStatement cb = conexion.prepareCall("insert into contactos( ID, Nombre, Edad , Email, NumeroDeTelefono) values('"+ct.getID()+"','"+ct.getNombre()+"','"+ct.getEdad()+"','"+ct.getEmail()+"','"+ct.getNumeroDeTelefono()+"')");
+                cb.execute();
+                
+                JOptionPane.showMessageDialog(null,"Contacto agregado","Mensaje sistems",1);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Error" + ex.toString(),"Mensaje sistems", 1);
+              
+            }
+    }
+    public void upContacto(){
+        
+    } 
+}
